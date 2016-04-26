b401252fd68bdd66d771cb212f5e67d37760d648
Razak Wasiu <madibalive@users.noreply.github.com>
2016-04-26 09:16:58 +0000

    fixed al typos

--- index.js
+++ index.js
@@ -1,132 +1,141 @@
 var twit = require('twit');
 var _ = require('lodash');
+var natural = require('natural');
+var secret = require("./secret.js");
 
-
-// importing my secret.json file
-var secret = require("./secret");
-
-// my secret.json file looks like this:
 var Twitter = new twit(secret);
+var tokenizer = new natural.WordTokenizer();
 
 //sample database ,improve to redis persistance store
 var contestants = [
-    { 'name': 'MAHAMA', 'votes': 10 },
-    { 'name': 'NANA', 'votes': 10 },
-    { 'name': 'INDOM', 'votes': 10 }
+  { 'name': 'MAHAMA', 'votes': 10 },
+  { 'name': 'NANA', 'votes': 10 },
+  { 'name': 'INDOM', 'votes': 10 }
 ]
 //sample database ,improve to redis persistance store
 
-var voterRegistration = []
-
+var voterRegistration = [];
 // we will randomly pick one of these items in this array
-var successMsg = [
 
-    "retweet so other can vote too.",
-    "Most likely.",
-    "vote is confirmed.",
-    "Thank you for voting ."
+var successMsg = [
+  "retweet so other can vote too.",
+  "Most likely.",
+  "vote is confirmed.",
+  "Thank you for voting ."
 ]
 var errorMsg = [
-    "Reply hazy, try again.",
-    "try again later.",
-
+  "Reply hazy, try again.",
+  "try again later.",
 ]
 
 var denyMsg = [
-    "Don't vote more once .",
-    "double votes ,gets rejected",
+  "Don't vote more once .",
+  "double votes ,gets rejected",
 ]
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 function matchRE(re, text) {
-    var wordArray = tokenizer.tokenize(text);
-    for (var i = 0; i < wordArray.length; i++) {
-        if (re.test(wordArray[i])) {
-            return true;
-        }
+  var wordArray = tokenizer.tokenize(text);
+  for (var i = 0; i < wordArray.length; i++) {
+    if (re.test(wordArray[i])) {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
+
 function post(content) {
-    twitter.post('statuses/update', { status: content }, function (err, data, response) {
-    })
+  console.log(content);
+  Twitter.post('statuses/update', { status: content }, function (err, data, response) {
+  })
 }
+
 function postHelp(mention) {
-    var msg = 'Hi,i am twitter voting bot,Tweet at me [HELP, OPTIONS,RESULT] to get started';
-    post(message);
+  var msg = '@' + mention + ' ' + 'Hi,i am twitter voting bot,Tweet at me [OPTIONS,RESULT] to get started';
+  post(msg);
 }
 
-function options(mention) {
-    var msg = '@' + mention + ' OPTIONS are [MAHAMA,NANA,NDOUM],reply with VOTE followed by one of the options ';
+function postOptions(mention) {
+  var msg = '@' + mention + ' OPTIONS are [MAHAMA,NANA,NDOUM],reply with VOTE followed by one of the options ';
+  post(msg);
 }
+
+function postResult(mention) {
+
+  var string1 = contestants[0].name + " : " + contestants[0].votes;
+  var string2 = contestants[1].name + " : " + contestants[1].votes;
+  var string3 = contestants[2].name + " : " + contestants[2].votes;
+
+  var msg = "@" + mention + " " + string1 + " ," + string2 + " " + string3;
+  post(msg)
+}
+
 function doVote(name, mention) {
-    var msg;
-    //has already voted 
-    var checkVoted = _.findKey(voterRegistration, { 'name': mention });
+  var msg;
+  //has already voted 
+  var checkVoted = _.findKey(voterRegistration, { 'name': mention });
 
-    if (!checkVoted) {
-        var contestant = _.find(contestants, { 'name': name });
-        constestant.vote = contestant.votes + 1;
+  if (!checkVoted) {
+    var a = _.findKey(contestants, { 'name': name });
+    contestants[a].votes = contestants[a].votes + 1;
 
-        msg = mention + ' Vote success'
-        post(message)
-        voterRegistration.push({ 'name': mention })
-        return
-    } else {
-        msg = mention + " Sorry you cant vote twice, or see admin if this is not an error";
-        post(msg);
-    }
-
-
+    msg = '@' + mention + ' ' + ' Vote success';
+    post(msg)
+    voterRegistration.push({ 'name': mention })
+    return
+  } else {
+    msg = "@" + mention + " Sorry you cant vote twice, or see admin if this is not an error";
+    post(msg);
+  }
 }
 
 function vote(mention, tweet) {
-    var mahamaRe = /^MAHAMA$/;
-    var nanaRe = /^NANA$/;
-    var induomRe = /^INDUOM$/
-    var wordArray = tokenizer.tokenize(tweet);
-    if (matchRE(mahamaRe, text)) {
-        doVote(mention)
-    } else if (matchRE(nanaRe, text)) {
-        doVote(mention)
-    } else if (matchRE(induomRe, text)) {
-        doVote(mention)
-    } else {
-        post('Please include a contestant name to vote for the person ');
-    }
+  var mahamaRe = /^MAHAMA$/;
+  var nanaRe = /^NANA$/;
+  var induomRe = /^INDUOM$/;
+  if (matchRE(mahamaRe, tweet)) {
+    doVote('MAHAMA', mention)
+  } else if (matchRE(nanaRe, tweet)) {
+    doVote('NANA', mention)
+  } else if (matchRE(induomRe, tweet)) {
+    doVote('NDOUM', mention)
+  } else {
+    var msg = '@' + mention + ' ' + 'Please include a contestant name to vote for the person,reply with HELP'
+    post(msg);
+  }
 }
+
+var stream = Twitter.stream('statuses/filter', { track: '#GhanaDecides' })
 
 // Call the stream function and pass in 'statuses/filter', our filter object, and our callback
-Twitter.stream('statuses/filter', { track: '#GhVoteBot' }, function (stream) {
+stream.on('tweet', function (tweet) {
 
-    // ... when we get tweet data...
-    stream.on('data', function (tweet) {
+  var mention = tweet.user.screen_name;
+  var text = tweet.text;
+  console.log(text);
 
-        var mention = tweet.user.screen_name;
-        var text = tweet.text;
+  // RegExes
+  var optionsRe = /^OPTIONS$/;
+  var helpRE = /^HELP$/;
+  var voteRE = /^VOTE$/;
+  var resultRE = /^RESULT$/;
 
-        // RegExes
-        var optionsRe = /^OPTIONS$/;
-        var helpRE = /^HELP$/;
-        var voteRE = /^VOTE$/;
-        var resultRE = /^RESULT$/;
-        var jokesRE = /^jokes$/;
+  if (matchRE(optionsRe, text)) {
+    postOptions(mention)
+  } else if (matchRE(helpRE, text)) {
+    postHelp(mention)
+  } else if (matchRE(voteRE, text)) {
+    vote(mention, text);
+  } else if (matchRE(resultRE, text)) {
+    postResult(mention)
+  } else {
+    post("Hey " + "@" + mention + "am a voting bot, tweet me with [HELP, OPTIONS ,RESULT] for more infor");
+  }
+});
 
-        if (matchRE(optionsRe, text)) {
-            postOptions(mention)
-        } else if (matchRE(helpRE, text)) {
-            postHelp(mention)
-        } else if (matchRE(voteRE, text)) {
-            vote(mention, text);
-        } else if (matchRE(resultRe, text)) {
-            postResult(mention)
-        } else {
-            post("Hey " + "@" + mention + "  tweet me with [HELP, OPTIONS ,RESULT] for more infor");
-        }
-
-        // ... when we get an error...
-        stream.on('error', function (error) {
-            //print out the error
-            console.log(error);
-        });
-    });
-
-}
+stream.on('error', function (error) {
+  //print out the error
+  console.log(error);
+});
---
+++ secret.js
@@ -0,0 +1,6 @@
+module.exports={
+    "consumer_key": "9PSUdq4j2QVOHmLGpsNqQXGB1",
+    "consumer_secret": "wdwSj7U9EqryiPLdmz6ObVD5voFdVAGAgHW3RADgnwg4HCElSJ",
+    "access_token": "724677562727825409-Rd6EfYKWcc5TUERwaILNJY0TzJ1MSAv",
+    "access_token_secret": "vjoVi8lqfrMiIZIX1dQ8OOsNrmAUFiazTYPxT7Rj7RFEG"
+};
